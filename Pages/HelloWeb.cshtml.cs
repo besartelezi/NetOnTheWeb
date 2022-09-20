2cbcff33a926357a0b6ffe3fd46ca51ab6d69881
Besart Elezi <besart.elezi@hotmail.com>
2022-09-20 10:00:59 +0200

    added HelloWeb page

---
+++ Pages/HelloWeb.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloWorld.Pages
+{
+    public class HelloWebModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
