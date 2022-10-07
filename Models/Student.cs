3152286880537a6d6180d358121dcbc9e0822193
Besart Elezi <besart.elezi@hotmail.com>
2022-10-07 11:29:12 +0200

    added Models folder and Student class

---
+++ Models/Student.cs
@@ -0,0 +1,32 @@
+﻿namespace NetOntheWeb.Models;
+public class Student 
+{
+    private int _id;
+    private string _name;
+    private int _classId;
+
+    public Student(int id, string name, int classId)
+    {
+        _id = id;
+        _name = name;
+        _classId = classId;
+    }
+
+    public int Id
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int ClassId
+    {
+        get => _classId;
+        set => _classId = value;
+    }
+}
