1ef3320f43d147b478879f64e0aec0e5ee03ca81
Besart Elezi <besart.elezi@hotmail.com>
2022-10-07 11:27:31 +0200

    removed students model from page

--- Pages/HelloWeb.cshtml.cs
+++ Pages/HelloWeb.cshtml.cs
@@ -1,12 +1,17 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HelloWorld.Pages
 {
+    
     public class HelloWebModel : PageModel
     {
         public void OnGet()
         {
+            
         }
+        
+        public string Message { get; set; } = "In page model: ";
+        
     }
 }
+
